8a13316ce32ef587788f05bc405bab889a51c1b3
lith453 <99thompsonp@gmail.com>
2025-03-01 15:22:11 -0600

    Initial commit

---
+++ DayOfTheWeek.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main () {
+    string day; //initialize string variable for day input
+   
+    cout << "Enter a day of the week: ";
+    cin >> day;
+
+    for (auto& day_alt : day) {     //function to convert day string to all lowercase to prevent input errors
+        day_alt = tolower(day_alt);
+    }
+    
+    int dayValue;                           //variable that will specify day as integer value for switch statement
+    string monday       = "monday";         //string variables that will be compared to day string
+    string tuesday      = "tuesday";
+    string wednesday    = "wednesday";
+    string thursday     = "thursday";
+    string friday       = "friday";
+    string saturday     = "saturday";
+    string sunday       = "sunday";
+
+    if (day == monday || day == tuesday || day == thursday) { //compares input day string to each day name 
+        dayValue = 1;                                         //to determine what day value to assign
+    } else if (day == wednesday) {
+        dayValue = 2;
+    } else if (day == friday) {
+        dayValue = 3;
+    } else (day == saturday || day == sunday) {
+        dayValue = 4;
+    }
+
+    switch(dayValue) {      //compares day value assigned and outputs appropriate response
+        case(1):
+            cout << "It's a weekday!";
+            break;
+        
+        case(2):
+            cout << "Hump day!";
+            break;
+        
+        case(3):
+            cout << "TGIF!";
+            break;
+        
+        case(4):
+            cout << "It's the weekend!";
+            break;
+    }
+    
+
+    return 0;
+}
